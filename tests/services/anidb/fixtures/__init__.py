eab6c105ec82bc41449ca0e2afc817c9a4785a26
Dean Gardiner <me@dgardiner.net>
2016-05-10 14:35:28 +1200

    Updated tests for new client + database structure

--- tests/services/anidb/fixtures/__init__.py
+++ tests/services/anidb/fixtures/__init__.py
@@ -3,11 +3,20 @@ from oem import Client
 import os
 import pytest
 
+from oem.providers import PackageProvider
+
 BASE_DIR = os.path.dirname(__file__)
 
 
 @pytest.fixture(scope="module")
 def client():
-    client = Client([os.path.join(BASE_DIR)])
+    client = Client(
+        provider=PackageProvider(
+            search_paths=[
+                os.path.join(BASE_DIR)
+            ],
+            use_installed_packages=False
+        )
+    )
     client.load_all()
     return client
