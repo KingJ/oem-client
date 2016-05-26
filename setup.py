191a09ac1b6df88a30ad69ddeb3e017b2878fe02
Dean Gardiner <me@dgardiner.net>
2016-05-26 14:31:45 +1200

    Updated requirements defined in [setup.py]

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ setup(
     author_email="me@dgardiner.net",
 
     install_requires=[
+        'oem-core>=1.0.0',
+        'oem-framework>=1.0.0',
+
         'appdirs>=1.4.0',
         'requests>=2.10.0',
         'semantic_version>=2.5.0'
