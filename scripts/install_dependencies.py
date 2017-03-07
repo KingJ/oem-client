72fb93d4ba523e82b95d07e6d344e60ff96f6198
Dean Gardiner <me@dgardiner.net>
2017-03-07 15:23:18 +1300

    Updated [tox.ini] and [.travis.yml], and cleaned up requirement definitions

--- scripts/install_dependencies.py
+++ scripts/install_dependencies.py
@@ -1,31 +1,32 @@
+from __future__ import absolute_import, division, print_function
+
 from argparse import ArgumentParser
 from subprocess import Popen
 import os
 
 
 DEPENDENCIES = [
-    ('flake8', [
-        'flake8==2.6.0'
-    ]),
-    ('py32', [
-        'coverage==3.7.1'
-    ]),
     ('develop', [
-        '-rrequirements_develop.txt',
-        '-rtests/requirements_develop.txt'
+        # Install development packages
+        '-rdevelop-requirements.txt',
+        '-rtests/develop-requirements.txt',
+
+        # Install extra packages
+        '-rrequirements.txt',
+        '-rtest-requirements.txt'
     ]),
     ('pip', [
         '-rrequirements.txt',
-        '-rtests/requirements.txt'
+        '-rtest-requirements.txt'
     ]),
     ('travis', [
-        '-rrequirements_vendor.txt',
-        '-rtests/requirements_vendor.txt',
-
         '--editable=git+https://github.com/OpenEntityMap/oem-framework.git@{BRANCH}#egg=oem-framework',
         '--editable=git+https://github.com/OpenEntityMap/oem-core.git@{BRANCH}#egg=oem-core',
         '--editable=git+https://github.com/OpenEntityMap/oem-format-json.git@{BRANCH}#egg=oem-format-json',
-        '--editable=git+https://github.com/OpenEntityMap/oem-storage-file.git@{BRANCH}#egg=oem-storage-file'
+        '--editable=git+https://github.com/OpenEntityMap/oem-storage-file.git@{BRANCH}#egg=oem-storage-file',
+
+        '-rrequirements.txt',
+        '-rtest-requirements.txt'
     ])
 ]
 
